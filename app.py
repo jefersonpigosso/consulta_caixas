8f2fbf836f939a02eab33ed785157e161e55e1e0
jefersonpigosso <152048108+jefersonpigosso@users.noreply.github.com>
2024-11-22 12:40:59 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+from streamlit import session_state as sts
+import pandas as pd
+
+st.set_page_config(page_title='Consulta Caixas', page_icon='📦')
+
+######################################################
+
+def lista_caixas():
+    sts.df = pd.read_excel(r'files/lista.xlsx', dtype='string')
+
+def consulta_caixa():
+
+    if sts.caixa in set(sts.df['Caixa']):
+        return True
+
+######################################################
+
+if 'df' not in sts:
+    lista_caixas()
+if 'caixa' not in sts:
+    sts.caixa = ''
+if 'status_caixa' not in sts:
+    sts.status_caixa = ''
+
+######################################################
+
+if len(sts.caixa) == 8:
+
+    if consulta_caixa():
+        st.toast(body=f'Separar!', icon='✅')
+
+    else:
+        st.toast(body=f'Armazenar!', icon='⚠️')
+    
+    sts.caixa = ''
+
+######################################################
+
+st.text_input(label='Caixa', key='caixa', max_chars=8)
+st.text(f'Total de caixas: {len(sts.df):,}'.replace(",", "."))
